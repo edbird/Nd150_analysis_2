cce88ed34c84f05f2e10e36883791483d29f83fc
Edward Birdsall <edward.birdsall@postgrad.manchester.ac.uk>
2021-01-20 16:44:23 +0000

    use min point struct

--- MeasureStuff/Macros/newLogLikFitter_mps_aux.h
+++ MeasureStuff/Macros/newLogLikFitter_mps_aux.h
@@ -12,10 +12,10 @@ void min_point_marker_helper
     TMarker *&mark_min_point_sysx_h,
     TLine *&line_min_point_sysx_l,
     TLine *&line_min_point_sysx_h,
-    double min_point[2],
-    double min_point_fake_data[2],
-    double min_point_sysx_l[2],
-    double min_point_sysx_h[2],
+    MinPoint min_point_data,
+    MinPoint min_point_fake_data,
+    MinPoint min_point_sysx_l,
+    MinPoint min_point_sysx_h,
     const bool ENABLE_MIN_POINT_SYSx,
     Int_t markstyle,
     Color_t markcolor,
@@ -31,25 +31,25 @@ void min_point_marker_helper
         double y_orig = 0.0;
         if(g_mode_fake_data == true)
         {
-            x_orig = min_point_fake_data[0];
-            y_orig = min_point_fake_data[1];
+            x_orig = min_point_fake_data.xi_31;
+            y_orig = min_point_fake_data.A;
         }
         else if(g_mode_fake_data == false)
         {
-            x_orig = min_point[0];
-            y_orig = min_point[1];
+            x_orig = min_point_data.xi_31;
+            y_orig = min_point_data.A;
         }
 
-        if((min_point_sysx_l[0] != 0.0) &&
-           (min_point_sysx_l[1] != 0.0))
+        if((min_point_sysx_l.xi_31 != 0.0) &&
+           (min_point_sysx_l.A != 0.0))
         {
-            double x_min_point = min_point_sysx_l[0] - min_point_fake_data[0] + x_orig;
-            double y_min_point = min_point_sysx_l[1] - min_point_fake_data[1] + y_orig;
+            double x_min_point = min_point_sysx_l.xi_31 - min_point_fake_data.xi_31 + x_orig;
+            double y_min_point = min_point_sysx_l.A - min_point_fake_data.A + y_orig;
 
             //std::cout << "DRAW MARK SYSxL" << std::endl;
-            std::cout << "SYSxL: " << min_point_sysx_l[0] << " " << min_point_sysx_l[1] << std::endl;
-            std::cout << "MIN_POINT: " << min_point[0] << " " << min_point[1] << std::endl;
-            std::cout << "MIN_POINT FAKE DATA: " << min_point_fake_data[0] << " " << min_point_fake_data[1] << std::endl;
+            std::cout << "SYSxL: " << min_point_sysx_l.xi_31 << " " << min_point_sysx_l.A << std::endl;
+            std::cout << "MIN_POINT: " << min_point_data.xi_31 << " " << min_point_data.A << std::endl;
+            std::cout << "MIN_POINT FAKE DATA: " << min_point_fake_data.xi_31 << " " << min_point_fake_data.A << std::endl;
             std::cout << "DRAWING: " << x_min_point << " " << y_min_point << std::endl;
 
             mark_min_point_sysx_l = new TMarker(x_min_point, y_min_point, markstyle);
@@ -61,16 +61,16 @@ void min_point_marker_helper
             line_min_point_sysx_l->SetLineWidth(1);
         }
 
-        if((min_point_sysx_h[0] != 0.0) &&
-           (min_point_sysx_h[1] != 0.0))
+        if((min_point_sysx_h.xi_31 != 0.0) &&
+           (min_point_sysx_h.A != 0.0))
         {
-            double x_min_point = min_point_sysx_h[0] - min_point_fake_data[0] + x_orig;
-            double y_min_point = min_point_sysx_h[1] - min_point_fake_data[1] + y_orig;
+            double x_min_point = min_point_sysx_h.xi_31 - min_point_fake_data.xi_31 + x_orig;
+            double y_min_point = min_point_sysx_h.A - min_point_fake_data.A + y_orig;
 
             //std::cout << "DRAW MARK SYSxH" << std::endl;
-            std::cout << "SYSxH: " << min_point_sysx_h[0] << " " << min_point_sysx_h[1] << std::endl;
-            std::cout << "MIN_POINT: " << min_point[0] << " " << min_point[1] << std::endl;
-            std::cout << "MIN_POINT FAKE DATA: " << min_point_fake_data[0] << " " << min_point_fake_data[1] << std::endl;
+            std::cout << "SYSxH: " << min_point_sysx_h.xi_31 << " " << min_point_sysx_h.A << std::endl;
+            std::cout << "MIN_POINT: " << min_point_data.xi_31 << " " << min_point_data.A << std::endl;
+            std::cout << "MIN_POINT FAKE DATA: " << min_point_fake_data.xi_31 << " " << min_point_fake_data.A << std::endl;
             std::cout << "DRAWING: " << x_min_point << " " << y_min_point << std::endl;
             mark_min_point_sysx_h = new TMarker(x_min_point, y_min_point, markstyle);
             mark_min_point_sysx_h->SetMarkerColorAlpha(markcolor, 1.0);
